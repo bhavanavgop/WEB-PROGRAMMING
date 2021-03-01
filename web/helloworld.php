cd9cc64a381af7ec04d7a9b63994e7ead8bbb2f2
bhavanavgop <76172342+bhavanavgop@users.noreply.github.com>
2021-03-01 16:10:11 +0530

    Add files via upload

---
+++ web/helloworld.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+<h1>hello world</h1>
+ <?php
+ echo "Hello World";
+ ?>
+</body>
+</html>
---
+++ web/studreg.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title> insert data in form </title>
+</head>
+<body>
+<form action = "getdata.php" method = "post">
+Name :
+<input type = "text" name = "textname">
+<br><br>
+Roll No :
+<input type = "text"name = "txtr_no">
+<br><br>
+Addrress :
+<text area name =  "add" type = "textarea"></textarea>
+<br><br>
+Contact No :
+<input type = "text" name = "txtc_no">
+<br><br>
+Email ID :
+<input type = "text" name = "textid">
+<br><br>
+<input type = "submit"name = "insert"value = "save">
+<input type ="reset"value = "cancel">
+</form>
+</body>
+</html>
